c7a23573608330c0f96ee11e66f866efbd191d7d
JANAKNAKUM <janaksilverwebbuzz@gmail.com>
2024-09-06 16:43:28 +0530

    section responsive

--- src/components/PreLoader.jsx
+++ src/components/PreLoader.jsx
@@ -1,6 +1,7 @@
 import '../style/PreLoader.css';
 import logo from '../images/Component 42.svg';
 const Loading = () => {
+   
   return (
     <div id="loader_container">
        <img src={logo} alt="loader" />
